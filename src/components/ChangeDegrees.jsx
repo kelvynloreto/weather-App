fc52956607f6203dfe9cb412d53b3a9d796b458d
kelvynloreto <loretokelvyn@gmail.com>
2022-07-29 23:19:55 -0400

    icons

--- src/components/ChangeDegrees.jsx
+++ src/components/ChangeDegrees.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-
+import celsius from '../icons/celsius.png'
+import farenheit from '../icons/farenheit.png'
 const ChangeDegrees = ({ tempConverter }) => {
   return (
     <button  onClick={tempConverter}>
-      <img src="/src/icons/celsius.png" alt="" />
+      <img src={celsius} alt="" />
       <box-icon name="transfer"></box-icon>
-      <img src="/src/icons/farenheit.png" alt="" />
+      <img src={farenheit} alt="" />
     </button>
   );
 };
--- src/components/NavIcon.jsx
+++ src/components/NavIcon.jsx
@@ -1,18 +1,22 @@
 import React from "react";
+import wind from '../icons/wind.png'
+import cloud from '../icons/cloud.png'
+import humedad from '../icons/humedad.png'
+
 
 const NavIcon = ({weather}) => {
   return (
     <nav>
       <p>
-        <img src="/src/icons/wind.png" alt="" />
+        <img src={wind} alt="" />
         {weather.wind?.speed}m/s
       </p>
       <p>
-        <img src="/src/icons/cloud.png" alt="" />
+        <img src={cloud} alt="" />
         {weather.clouds?.all}%
       </p>
       <p>
-        <img src="/src/icons/humedad.png" alt="" />
+        <img src={humedad} alt="" />
         {weather.main?.humidity}%
       </p>
     </nav>
